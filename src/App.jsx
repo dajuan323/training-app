4f0e7932aeb06d36719fdc4f5266727decdf91b0
dajuan323 <macaro.martin@gmail.com>
2023-04-01 20:03:17 -0400

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,103 +1,17 @@
 import React from "react";
 import "./App.css";
+import TopNavBar from "./components/navbars/TopNavBar";
 
 function App() {
   return (
     <React.Fragment>
-      <nav
-        className="navbar navbar-expand-md navbar-dark bg-dark"
-        aria-label="Fourth navbar example"
-      >
-        <div className="container">
-          <a className="navbar-brand" href="/">
-            <img
-              src="https://pw.sabio.la/images/Sabio.png"
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
-              alt="Sabio"
-            />
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarsExample04"
-            aria-controls="navbarsExample04"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-
-          <div className="collapse navbar-collapse" id="navbarsExample04">
-            <ul className="navbar-nav me-auto mb-2 mb-md-0">
-              <li className="nav-item">
-                <button className="nav-link px-2 text-white link-button">
-                  Home
-                </button>
-              </li>
-              <li className="nav-item">
-                <button className="nav-link px-2 text-white link-button">
-                  Friends
-                </button>
-              </li>
-              <li className="nav-item">
-                <button
-                  href="#"
-                  className="nav-link px-2 text-white link-button"
-                >
-                  Jobs
-                </button>
-              </li>
-              <li className="nav-item">
-                <button
-                  href="#"
-                  className="nav-link px-2 text-white link-button"
-                >
-                  Tech Companies
-                </button>
-              </li>
-              <li className="nav-item">
-                <button
-                  href="#"
-                  className="nav-link px-2 text-white link-button"
-                >
-                  Events
-                </button>
-              </li>
-              <li className="nav-item">
-                <button
-                  href="#"
-                  className="nav-link px-2 text-white link-button"
-                >
-                  Test and Ajax Call
-                </button>
-              </li>
-            </ul>
-            <div className="text-end">
-              <a
-                href="/"
-                className="align-items-center mb-2 me-2 mb-lg-0 text-white text-decoration-none"
-              >
-                Unknow User
-              </a>
-              <button type="button" className="btn btn-outline-light me-2">
-                Login
-              </button>
-              <button type="button" className="btn btn-warning">
-                Register
-              </button>
-            </div>
-          </div>
-        </div>
-      </nav>
+      <TopNavBar />
 
       <main role="main">
         <div className="container">
           <div className="p-5 mb-4 bg-light rounded-3">
             <div className="container-fluid py-5">
-              <h1 className="display-5 fw-bold">Hello, world!</h1>
+              <h1 className="display-5 fw-bold">Hello, Students!</h1>
               <p className="col-md-8 fs-4">
                 The button below should make an ajax call when clicked. Look at
                 your task details for more infomation about the specifics.
