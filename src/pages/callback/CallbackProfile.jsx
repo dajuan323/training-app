f5dbd1e6d695542cd77ebee6a0afe661333b1dc2
dajuan323 <macaro.martin@gmail.com>
2023-12-03 18:15:06 -0500

    Saving Card example

--- src/pages/callback/CallbackProfile.jsx
+++ src/pages/callback/CallbackProfile.jsx
@@ -12,12 +12,10 @@ function CallbackProfile(props) {
   const worker = props.workerData;
   const userId = useParams();
   const databox = boatList;
-  const worker2 = databox[userId];
-  console.log(`databox: ${databox}`);
-  _logger(`worker ${worker.name}`);
-  _logger(`userId ${userId}`);
-  _logger(`databox ${databox}`);
-  _logger(`worker2 ${worker2}`);
+  const worker2 = databox.filter(
+    (person) => person.id === parseInt(userId.userId)
+  )[0];
+  console.log(worker2);
   const location = useLocation();
   const { payload, type } = location.state;
   const [profileState, setProfile] = useState(payload);
@@ -31,21 +29,36 @@ function CallbackProfile(props) {
       });
     }
   }, []);
+  _logger(`worker ${worker.name}`);
+  _logger(userId.userId);
+  _logger(databox);
+  _logger(`worker2 ${worker2}`);
   console.log(`worker: ${worker}`);
   console.log(`payload: ${payload.name}, type: ${type}`);
   console.log(`location: ${useLocation()}`);
   console.log(profileState);
 
   return (
-    <Card>
-      <ul>
-        <li>name: {profileState.name}</li>
-        <li>age: {profileState.age}</li>
-        <li>id: {profileState.id}</li>
-        <li>degree: {profileState.degree}</li>
-        <li>job: {profileState.job}</li>
-      </ul>
-    </Card>
+    <>
+      <Card>
+        <ul>
+          <li>name: {profileState.name}</li>
+          <li>age: {profileState.age}</li>
+          <li>id: {profileState.id}</li>
+          <li>degree: {profileState.degree}</li>
+          <li>job: {profileState.job}</li>
+        </ul>
+      </Card>
+      <Card>
+        <ul>
+          <li>name: {worker2.name}</li>
+          <li>age: {worker2.age}</li>
+          <li>id: {worker2.id}</li>
+          <li>degree: {worker2.degree}</li>
+          <li>job: {worker2.job}</li>
+        </ul>
+      </Card>
+    </>
   );
 }
 
